e507af619e7160c751894e2169d39dcce6d8338e
Radu Grozavu <grozavu.radu@gmail.com>
2019-11-17 10:57:37 +0200

    no message

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-session_start();
+$homepage = file_get_contents('https://serviciul1.herokuapp.com/');
+echo $homepage;
 ?>
-<!DOCTYPE html>
-<html>
-<body>
-
-<?php
-// Echo session variables that were set on previous page
-echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
-echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
-?>
-
-</body>
-</html>
