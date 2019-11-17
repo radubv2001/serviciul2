1ed2bbda051888eb877a1c121d965a0ee56b921a
Radu Grozavu <grozavu.radu@gmail.com>
2019-11-17 10:46:37 +0200

    no message

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo ' show me the money';
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+?>
+
+</body>
+</html>
